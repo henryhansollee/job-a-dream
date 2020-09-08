fda8f7fa917deb89dadf38064ac276c95bf7c0aa
이한솔 <henryhansollee@gmail.com>
2020-09-09 01:48:06 +0900

    Complete API linkage S03P22B107-49

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,7 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer, { rootSaga } from './modules';
 
 const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
 sagaMiddleware.run(rootSaga);
