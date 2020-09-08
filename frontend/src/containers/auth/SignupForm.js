aa732d43356cdbe0a904e82a65bf02a8b6c54fc0
이한솔 <henryhansollee@gmail.com>
2020-09-09 03:02:15 +0900

    Complete signup S03P22B107-50

--- frontend/src/containers/auth/SignupForm.js
+++ frontend/src/containers/auth/SignupForm.js
@@ -2,13 +2,16 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, signup } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
+import { withRouter } from 'react-router-dom';
 
-const SignupForm = () => {
+const SignupForm = ({ history }) => {
   const dispatch = useDispatch();
-  const { form, auth, authError } = useSelector(({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.signup,
     auth: auth.auth,
-    authError: auth.authError
+    authError: auth.authError,
+    user: user.user,
   }));
 
   const onChange = e => {
@@ -44,8 +47,15 @@ const SignupForm = () => {
     if (auth) {
       console.log('회원가입 성공');
       console.log(auth);
+      dispatch(check());
     }
-  }, [auth, authError]);
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      history.push('/');
+    }
+  }, [history, user]);
 
   return (
     <AuthForm
