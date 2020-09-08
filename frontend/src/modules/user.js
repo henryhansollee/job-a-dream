47425c2797c0e132cb28b92dadb72b85d8dcb69d
이한솔 <henryhansollee@gmail.com>
2020-09-09 00:58:46 +0900

    Update user redux module

--- frontend/src/modules/user.js
+++ frontend/src/modules/user.js
@@ -1,8 +1,19 @@
 import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
+import { takeLatest } from 'redux-saga/effects';
+import createRequestSaga, { createRequestActionTypes } from '../lib/createRequestSaga';
+import * as userAPI from '../lib/api/user';
 
 const CHANGE_FIELD = 'user/CHANGE_FIELD';
 const INITIALZE_FORM ='user/INITIALIZE_FORM';
+
+const [SIGNUP, SIGNUP_SUCESS, SIGNUP_FAILURE] = createRequestActionTypes(
+  'user/SIGNUP',
+);
+
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes(
+  'user/login',
+);
 
 export const changeField = createAction(
   CHANGE_FIELD,
@@ -15,6 +26,23 @@ export const changeField = createAction(
 
 export const initializeForm = createAction(INITIALZE_FORM, form => form);
 
+export const signup = createAction(SIGNUP, ({ email, password }) => ({
+  email,
+  password,
+}));
+
+export const login = createAction(LOGIN, ({ email, password }) => ({
+  email,
+  password,
+}));
+
+const signupSaga = createRequestSaga(SIGNUP, userAPI.signup);
+const loginSaga = createRequestSaga(LOGIN, userAPI.login);
+export function* userSaga() {
+  yield takeLatest(SIGNUP, signupSaga);
+  yield takeLatest(LOGIN, loginSaga);
+}
+
 const initialState = {
   signup: {
     email: '',
@@ -25,6 +53,8 @@ const initialState = {
     email: '',
     password: '',
   },
+  user: null,
+  userError: null,
 };
 
 const user = handleActions(
@@ -36,6 +66,25 @@ const user = handleActions(
     [INITIALZE_FORM]: (state, { payload: form }) => ({
       ...state,
       [form]: initialState[form],
+      userError: null,
+    }),
+    [SIGNUP_SUCCESS]: (state, { payload: user }) => ({
+      ...state,
+      userError: null,
+      user,
+    }),
+    [SIGNUP_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      userError: error,
+    }),
+    [LOGIN_SUCCESS]: (state, { payload: user }) => ({
+      ...state,
+      userError: null,
+      user,
+    }),
+    [LOGIN_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      userError: error,
     }),
   },
   initialState,
