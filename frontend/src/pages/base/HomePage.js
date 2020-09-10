cafdec8402034dd8b4bcb67676e30c564488bf82
이한솔 <henryhansollee@gmail.com>
2020-09-10 21:14:13 +0900

    Create Home scroll responsive view
    Former-commit-id: 2a793e8761749ceca6c44e69e5f0f2f3557eeaf1

--- frontend/src/pages/base/HomePage.js
+++ frontend/src/pages/base/HomePage.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import Header from '../../components/common/Header';
-import Footer from '../../components/common/Footer';
+import ReactFullpage from '@fullpage/react-fullpage';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+
+const HomeSection1 = styled.div`
+  background: ${palette.gray[0]};
+`;
+
+const HomeSection2 = styled.div`
+  background: ${palette.gray[1]};
+`;
+
+const HomeSection3 = styled.div`
+  background: ${palette.gray[2]};
+`;
 
 const HomePage = () => {
   return (
     <>
-      <Header />
-      <div>홈</div>
-      <Footer />
-    </>
+      <ReactFullpage
+        licenseKey = {'YOUR_KEY_HERE'}
+        scrollingSpeed = {1000}
+        render={() => {
+          return (
+            <ReactFullpage.Wrapper>
+              <HomeSection1 className="section">
+              </HomeSection1>
+              <HomeSection2 className="section">
+              </HomeSection2>
+              <HomeSection3 className="section">
+              </HomeSection3>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+    />
+  </>
   )
 };
 
