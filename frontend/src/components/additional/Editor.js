3a16d0cd35866d02c1397014b8e8af4611b7853e
이한솔 <henryhansollee@gmail.com>
2020-09-11 14:43:23 +0900

    Create editor component
    Former-commit-id: a801ae67e61a6df3ef58fdf3bec0cd8c3f6bfdce

---
+++ frontend/src/components/additional/Editor.js
@@ -0,0 +1,63 @@
+import React, { useRef, useEffect } from 'react';
+import Quill from 'quill';
+import 'quill/dist/quill.bubble.css';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+import Responsive from '../common/Responsive';
+
+const EditorBlcok = styled(Responsive)`
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+`;
+
+const TitleInput = styled.input`
+  font-size: 3rem;
+  outline: none;
+  padding-bottom: 0.5rem;
+  border: none;
+  border-bottom: 1px soild ${palette.gray[0]};
+  margin-bottom: 2rem;
+`;
+
+const QuillWrapper = styled.div`
+  .ql-editor {
+    padding: 0;
+    min-height: 320px;
+    font-size: 1.125rem;
+    line-height: 1.5;
+  }
+  .ql-editor .ql-blank::before {
+    left: 0px;
+  }
+`;
+
+const Editor = () => {
+  const qillElement = useRef(null);
+  const quillInstance = useRef(null);
+
+  useEffect(() => {
+    quillInstance.current = new Quill(quillElement.current, {
+      theme: 'bubble',
+      placeholder: '내용을 작성하세요...',
+      modules: {
+        toolbar: [
+          [{ header: '1' }, { header:'2'} ],
+          ['bold', 'italic', 'underline', 'strike'],
+          [{ list: 'orderd' }, { list: 'bullet' }],
+          ['blockquote', 'code-block', 'link', 'image'],
+        ],
+      },
+    });
+  }), [];
+
+  return (
+    <EditorBlcok>
+      <TitleInput placeholder="제목을 입력하세요" />
+      <QuillWrapper>
+        <div ref={quillElement} />
+      </QuillWrapper>
+    </EditorBlcok>
+  );
+};
+
+export default Editor;
