55ecb526d624d918a451265510a1c1a5f44a0cc8
이한솔 <henryhansollee@gmail.com>
2020-09-15 20:35:00 +0900

    Apply fullpage
    Former-commit-id: dee78c879567afc02ec91715209c7dabd2fa9fd7

--- vue-frontend/frontend/src/pages/Home.vue
+++ vue-frontend/frontend/src/pages/Home.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>홈</div>
+  <div>
+    <full-page ref="fullpage" :options="options">
+      <div class="section fullpage-section1">
+      </div>
+      <div class="section fullpage-section2">
+      </div>
+      <div class="section fullpage-section3">
+      </div>
+    </full-page>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+      data() {
+        return {
+          options: {
+            afterLoad: this.afterLoad,
+          }
+        }
+      },
+    }
 </script>
 
 <style>
-
+.fullpage-section1 {
+  background-color: cadetblue;
+}
+.fullpage-section2 {
+  background-color: azure;
+}
+.fullpage-section3 {
+  background-color: antiquewhite;
+}
 </style>
