45fe20c5275a3de09d55d7a93e9ba5d2b65c4348
이한솔 <henryhansollee@gmail.com>
2020-09-15 20:29:00 +0900

    Apply router
    Former-commit-id: 06c5edb9103604d6bcbf7f7f40c780f7097c6ebd

--- vue-frontend/frontend/src/router/index.js
+++ vue-frontend/frontend/src/router/index.js
@@ -1,23 +1,83 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from "vue"
+import VueRouter from "vue-router"
+
+// home
+import Home from "../pages/Home"
+
+// auth
+import Signup from "../pages/auth/Signup"
+
+// interview
+import InterviewList from '../pages/interview/InterviewList'
+import InterviewCreate from '../pages/interview/InterviewCreate'
+import InterviewDetail from '../pages/interview/InterviewDetail'
+import InterviewUpdate from '../pages/interview/InterviewUpdate'
+
+// community
+import CommunityList from '../pages/community/CommunityList'
+import CommunityCreate from '../pages/community/CommunityCreate'
+import CommunityDetail from '../pages/community/CommunityDetail'
+import CommunityUpdate from '../pages/community/CommunityUpdate'
 
 Vue.use(VueRouter);
 
 const routes = [
+  // home
   {
     path: "/",
     name: "Home",
     component: Home
   },
+
+  // auth
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/signup",
+    name: "Signup",
+    component: Signup
+  },
+
+  // interview
+  {
+    path: "/ilist",
+    name: "InterviewList",
+    component: InterviewList
+  },
+  {
+    path: "/icreate",
+    name: "InterviewCreate",
+    component: InterviewCreate
+  },
+  {
+    path: "/idetail",
+    name: "InterviewDetail",
+    component: InterviewDetail
+  },
+  {
+    path: "/iupdate",
+    name: "InterviewUpdate",
+    component: InterviewUpdate
+  },
+
+  // community
+  {
+    path: "/clist",
+    name: "CommunityList",
+    component: CommunityList
+  },
+  {
+    path: "/ccreate",
+    name: "CommunityCreate",
+    component: CommunityCreate
+  },
+  {
+    path: "/cdetail",
+    name: "CommunityDetail",
+    component: CommunityDetail
+  },
+  {
+    path: "/cupdate",
+    name: "CommunityUpdate",
+    component: CommunityUpdate
   }
 ];
 
