8ea445604acab058f76b1b7f8ac94243b7719cf4
이한솔 <henryhansollee@gmail.com>
2020-09-17 21:44:10 +0900

    Link views
    Former-commit-id: ffc98b9a7e2d87ce111474f8d5165d27034fd345

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,6 +7,16 @@ import Home from '../views/Home.vue'
 // auth
 import Signup from '../views/auth/SignupView'
 
+// interview
+import InterviewListView from '../views/interview/InterviewListView'
+import InterviewCreateView from '../views/interview/InterviewCreateView'
+import InterviewDetailView from '../views/interview/InterviewDetailView'
+
+// community
+import CommunityListView from '../views/community/CommunityListView'
+import CommunityCreateView from '../views/community/CommunityCreateView'
+import CommunityDetailView from '../views/community/CommunityDetailView'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -22,6 +32,40 @@ const routes = [
     path: '/signup',
     name: 'Signup',
     component: Signup
+  },
+
+  // interview
+  {
+    path: '/interview/list',
+    name: 'InterviewListView',
+    component: InterviewListView
+  },
+  {
+    path: '/interview/create',
+    name: 'InterviewCreateView',
+    component: InterviewCreateView
+  },
+  {
+    path: '/interview/detail',
+    name: 'InterviewDetailView',
+    component: InterviewDetailView
+  },
+
+  // community
+  {
+    path: '/community/list',
+    name: 'CommunityListView',
+    component: CommunityListView
+  },
+  {
+    path: '/community/create',
+    name: 'CommunityCreateView',
+    component: CommunityCreateView
+  },
+  {
+    path: '/community/detail',
+    name: 'CommunityDetailView',
+    component: CommunityDetailView
   },
 ]
 
