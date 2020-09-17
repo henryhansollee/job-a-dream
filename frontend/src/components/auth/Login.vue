a0139eac7ece109ad1be5f07659567c3b7e80f56
이한솔 <henryhansollee@gmail.com>
2020-09-17 22:46:45 +0900

    Create temp login UI
    Former-commit-id: 702ba1062787b1eae61e6270e3c8319ce4057df8

--- frontend/src/components/auth/Login.vue
+++ frontend/src/components/auth/Login.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
-    <h1>Login</h1>
-    <div>
-      <input type="email" placeholder="email" v-model="loginData.email">
-      <input type="password" placeholder="password" v-model="loginData.password">
-      <button @click="login(loginData)">로그인</button>
-      <router-link to="/signup">회원가입</router-link>
+  <div class="login-block">
+    <h3 class="text-center pt-5">Login</h3>
+    <div class="inner-block">
+      <div class="d-flex flex-column">
+        <input class="m-1" type="email" placeholder="email" v-model="loginData.email">
+        <input class="m-1" type="password" placeholder="password" v-model="loginData.password">
+        <button class="m-1" @click="login(loginData)">로그인</button>
+        <router-link class="text-right text-dark text-decoration-none" to="/signup">회원가입</router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -30,5 +32,15 @@ export default {
 </script>
 
 <style>
-
+.login-block {
+  width: 25rem;
+  height: 20rem;
+  background-color: #fffcf0;
+}
+.inner-block {
+  margin-left: 20%;
+  width: 15rem;
+  height: 10rem;
+  background-color: #fffcf0;
+}
 </style>
