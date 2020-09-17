39126f475ba43d8ed8215c5512178785243a0d84
편재호 <pyoun820@naver.com>
2020-09-17 09:55:13 +0900

    add NLP code
    Former-commit-id: 7ee6c4f8196eb652ad59a3677be49f89abbc12d4

--- AI/AI_code/STT_ASYNC_version.py
+++ AI/AI_code/STT_ASYNC_version.py
@@ -1,27 +1,50 @@
 from google.cloud import storage
 from google.cloud import speech
 from google.cloud.speech import enums
-from google.cloud.speech import types
-
+from konlpy.tag import Okt
+from collections import defaultdict, Counter
+from wordcloud import WordCloud
+from matplotlib import pyplot as plt
+okt = Okt()
+word_collect = defaultdict(int)
 def transcribe_gcs(gcs_uri):
     """Asynchronously transcribes the audio file specified by the gcs_uri."""
     client = speech.SpeechClient()
-    audio = types.RecognitionAudio(uri=gcs_uri)
+    audio = {"uri" : gcs_uri}
     audio_channel_count = 2
 
-    config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.ENCODING_UNSPECIFIED,
-        audio_channel_count= audio_channel_count,
-        sample_rate_hertz=44100,
-        language_code='ko-KR')
+    config = {
+        "encoding":enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        "audio_channel_count": audio_channel_count,
+        "sample_rate_hertz":44100,
+        "language_code":'ko-KR',
+        # "enable_automatic_punctuation":True,
+        "enable_word_time_offsets": True,
+    }
+
     operation = client.long_running_recognize(config, audio)
     print('Waiting for operation to complete...')
     response = operation.result()
-    # Each result is for a consecutive portion of the audio. Iterate through
-    # them to get the transcripts for the entire audio file.
+
+    ment, confidence_avg = '', 0
+    count_sentence = 0
     for result in response.results:
-        print(u'Transcript: {}'.format(result.alternatives[0].transcript))
-        print('Confidence: {}'.format(result.alternatives[0].confidence))
+        alternative = result.alternatives[0]
+        text, conf = format(alternative.transcript), format(alternative.confidence)
+        ment += text + ' '; confidence_avg += float(conf); count_sentence += 1
+    for Noun in okt.nouns(ment):
+        word_collect[Noun] += 1
+    print(ment)
+    print(confidence_avg/count_sentence)
+    print(Counter(word_collect).most_common(10))
+    # data = Counter(word_collect).most_common(10)
+    # wordcloud로 만들기
+    # print(data)
+    # wordcloud = WordCloud(font_path='C:/Windows/Fonts/malgun.ttf', background_color='white', colormap="Accent_r",
+    #                       width=700, height=700).generate_from_frequencies(dict(data))
+    # plt.imshow(wordcloud)
+    # plt.axis('off')
+    # plt.show()
 
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
@@ -41,5 +64,5 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
         )
     )
 
-upload_blob("default", "the", "name")
+# upload_blob("default", "the", "name")
 transcribe_gcs('gs://speech2text-store/my-file')
