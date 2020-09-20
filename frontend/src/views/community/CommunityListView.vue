879c93f24e9960597472115020de6ec180af2614
이한솔 <henryhansollee@gmail.com>
2020-09-20 20:14:55 +0900

    Update community list
    Former-commit-id: b688db16f9f0a0a6b0be86bce25bef58407de574

--- frontend/src/views/community/CommunityListView.vue
+++ frontend/src/views/community/CommunityListView.vue
@@ -1,26 +1,27 @@
 <template>
   <div>
     <Header />
-    <h1>CommunityListView</h1>
-    <router-link to="/community/create">글 쓰기</router-link>
-    <table class="table">
-      <thead class="thead-dark">
-        <tr>
-          <th scope="col">번호</th>
-          <th scope="col">제목</th>
-          <th scope="col">내용</th>
-          <th scope="col">태그</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="community in communitys" :key="community.id">
-          <th scope="row">{{ community.id }}</th>
-          <td><router-link :to="`/community/detail/${community.id}`">{{ community.title }}</router-link></td>
-          <td>{{ community.content }}</td>
-          <td>{{ community.update_tag }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="container mt-5">
+      <router-link class="btn btn-dark mb-2" to="/community/create">글 쓰기</router-link>
+      <table class="table">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col" class="text-center">번호</th>
+            <th scope="col" class="text-center">제목</th>
+            <th scope="col" class="text-center">내용</th>
+            <th scope="col" class="text-center">태그</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="community in communitys" :key="community.id">
+            <th scope="row" class="text-center">{{ community.id }}</th>
+            <td class="text-center"><router-link :to="`/community/detail/${community.id}`">{{ community.title }}</router-link></td>
+            <td class="text-center">{{ community.content }}</td>
+            <td class="text-center">{{ community.update_tag }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
