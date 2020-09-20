178a769ccc93fe2250c4d6c9bf72a05f111d455e
이한솔 <henryhansollee@gmail.com>
2020-09-20 18:14:46 +0900

    Update signup modal
    Former-commit-id: 2ea0ecda87e9d41ffdf580f957d1dad332da0095

--- frontend/src/components/auth/LoginSignup.vue
+++ frontend/src/components/auth/LoginSignup.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login-block">
     <h3 class="text-center pt-5 mb-3">로그인</h3>
-    <div class="inner-block">
+    <div class="login-inner-block">
       <router-link v-if="isLoggedIn" to="/interview/list/" class="m-1">시작하기</router-link>
       <div v-else class="d-flex flex-column">
         <b-form-input type="email" placeholder="이메일" v-model="loginData.email" class="mb-1" />
@@ -11,12 +11,17 @@
       </div>
     </div>
     <div>
-      <b-modal id="modal-1" title="BootstrapVue">
-        <div>
-          <input type="text" placeholder="username" v-model="signupData.username">
-          <input type="email" placeholder="email" v-model="signupData.email">
-          <input type="password" placeholder="password1" v-model="signupData.password">
-          <button @click="signup(signupData)">회원가입</button>
+      <b-modal id="modal-1" title="BootstrapVue" hide-header="true" hide-footer="true">
+        <div class="signup-block">
+          <div class="signup-inner-block d-flex flex-column">
+            <h3 class="text-center mb-3 mt-3">회원가입</h3>
+            <div class="d-flex flex-column">
+              <b-form-input type="text" placeholder="이름" v-model="signupData.username" class="mb-1" />
+              <b-form-input type="email" placeholder="이메일" v-model="signupData.email" class="mb-1" />
+              <b-form-input type="password" placeholder="패스워드" v-model="signupData.password" class="mb-1" />
+              <b-button variant="dark" @click="signup(signupData)" class="mb-1">회원가입</b-button>
+            </div>
+          </div>
         </div>
       </b-modal>
     </div>
@@ -57,9 +62,21 @@ export default {
   background-color: #fffcf0;
   max-width: 500px;
 }
-.inner-block {
+.login-inner-block {
   margin-left: 20%;
   width: 60%;
+  height: 10rem;
+  background-color: #fffcf0;
+}
+.signup-block {
+  margin: 0;
+  width: 100%;
+  height: 17rem;
+  background-color: #fffcf0;
+}
+.signup-inner-block {
+  margin-left: 25%;
+  width: 50%;
   height: 10rem;
   background-color: #fffcf0;
 }
