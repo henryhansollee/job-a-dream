652be8c0176df79908bdeec513177464084e23d9
최보근 <bogun11904@naver.com>
2020-09-22 02:58:24 +0900

    Modify urls and refactoring serializer
    Former-commit-id: ed934a1f30c7f83f5995c66e73a31a26f5da32cf

--- backend/videos/views.py
+++ backend/videos/views.py
@@ -1,16 +1,27 @@
 from rest_framework.decorators import permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from .models import Video
 from .serializers import VideoSerializer
 
-@permission_classes([IsAuthenticatedOrReadOnly])
-@authentication_classes([JSONWebTokenAuthentication])
-class VideoListAPI(ListCreateAPIView):
-    queryset = Video.objects.all()
-    serializer_class = VideoSerializer
+
+class VideoListAPI(APIView):
+
+    def get(self, request):
+        serializer = VideoSerializer(Video.objects.filter(writer=request.user), many=True)
+        print(serializer)
+        return Response(serializer.data, status=200)
+    
+    def post(self, request):
+        serializer = VideoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(writer=request.user)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
 @permission_classes([IsAuthenticated])
 @authentication_classes([JSONWebTokenAuthentication])
