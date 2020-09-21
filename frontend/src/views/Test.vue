f10b525451e26ac12aae35f5a75e796727e442fa
이한솔 <henryhansollee@gmail.com>
2020-09-21 22:01:55 +0900

    Create scroll top button S03P23B107-16
    Former-commit-id: 281b2dbabd21ab0aaecaf8892aeed0206785943f

--- frontend/src/views/Test.vue
+++ frontend/src/views/Test.vue
@@ -1,14 +1,18 @@
 <template>
-  <div>
+  <div style="height: 2000px">
     <h1>TEST</h1>
+    <bbb />
   </div>
 </template>
 
 <script>
+import bbb from '../components/scroll/TopButton'
+
 export default {
-  name: "TEST",
-  componet: {
-    
+  name: "Test",
+  components: {
+    bbb,
+
   }
 }
 </script>
