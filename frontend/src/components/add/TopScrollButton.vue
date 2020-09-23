0f62d8a1f2821718d41e3a139fcea225e1a9e9c5
임기람 <rlfka@naver.com>
2020-09-23 14:19:26 +0900

    change design
    Former-commit-id: ef38c141c8a0d1c63defe5d46516df4afb1f330b

--- frontend/src/components/add/TopScrollButton.vue
+++ frontend/src/components/add/TopScrollButton.vue
@@ -1,36 +1,38 @@
 <template>
-    <a @click="scrollTop" v-show="visible" class="bottom-right">
-      <button>TOP</button>
-    </a>
+  <a @click="scrollTop" v-show="visible" class="bottom-right">
+    <button class="basic-btn totheTop" style="border-radius:4px;">
+      <i class="fas fa-arrow-up"></i>
+    </button>
+  </a>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      visible: true
-    }
+      visible: true,
+    };
   },
   methods: {
     scrollTop() {
       this.intervalId = setInterval(() => {
         if (window.pageYOffset === 0) {
-          clearInterval(this.intervalId)
+          clearInterval(this.intervalId);
         }
-        window.scroll(0, window.pageYOffset - 50)
-      }, 10)
+        window.scroll(0, window.pageYOffset - 50);
+      }, 10);
     },
     scrollListener() {
-      this.visible = window.scrollY > 0
-    }
+      this.visible = window.scrollY > 0;
+    },
   },
   mounted() {
-    window.addEventListener('scroll', this.scrollListener)
+    window.addEventListener("scroll", this.scrollListener);
   },
   beforeDestroy() {
-    window.removeEventListener('scroll', this.scrollListener)
-  }
-}
+    window.removeEventListener("scroll", this.scrollListener);
+  },
+};
 </script>
 
 <style scoped>
@@ -39,5 +41,23 @@ export default {
   bottom: 20px;
   right: 20px;
   cursor: pointer;
+}
+.totheTop {
+  position: fixed;
+  bottom: 1%;
+  right: 8%;
+  cursor: pointer;
+  width: 35px;
+  height: 35px;
+  background-color: transparent;
+  border: 0.5px solid #bbb3b3;
+  border-radius: 20px;
+  color: #1d1c1c;
+  box-shadow: 0 10px 20px -8px #b2b0b3;
+  font-weight: 100;
+  transition: all 0.3s ease;
+}
+.totheTop:hover {
+  transform: translateY(-3px);
 }
 </style>
