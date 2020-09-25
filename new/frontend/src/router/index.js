d80faac578bb61186547769951393f06f01f3ced
이한솔 <henryhansollee@gmail.com>
2020-09-25 14:46:09 +0900

    Delete lists
    Former-commit-id: 405b812d39d1eeabdebc2f125ff5dcf2f09269ca

--- new/frontend/src/router/index.js
+++ new/frontend/src/router/index.js
@@ -4,13 +4,6 @@ import VueRouter from 'vue-router'
 // Home
 import Home from '../views/Home.vue'
 
-// Lists
-import AudioListView from '../views/lists/AudioListView'
-import CoverLetterListView from '../views/lists/CoverLetterListView'
-import FullCourseListView from '../views/lists/FullCourseListView'
-import VideoListView from '../views/lists/VideoListView'
-import YoutubeListView from '../views/lists/YoutubeListView'
-
 Vue.use(VueRouter)
 
 const routes = [
@@ -19,33 +12,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-
-  // Lists
-  {
-    path: '/audio/list',
-    name: 'AudioListView',
-    component: AudioListView
-  },
-  {
-    path: '/coverletter/list',
-    name: 'CoverLetterListView',
-    component: CoverLetterListView
-  },
-  {
-    path: '/fullcourse/list',
-    name: 'FullCourseListView',
-    component: FullCourseListView
-  },
-  {
-    path: '/video/list',
-    name: 'VideoListView',
-    component: VideoListView
-  },
-  {
-    path: '/youtube/list',
-    name: 'YoutubeListView',
-    component: YoutubeListView
   },
 ]
 
