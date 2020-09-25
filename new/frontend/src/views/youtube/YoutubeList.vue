5e984202c65b2f771b17a055b4f57812a49b51cf
임기람 <rlfka@naver.com>
2020-09-25 17:34:36 +0900

    search videos
    Former-commit-id: f414923fea6b0ad5e7d9937bbfb8840f2671fdfa

--- new/frontend/src/views/youtube/YoutubeList.vue
+++ new/frontend/src/views/youtube/YoutubeList.vue
@@ -19,9 +19,49 @@
 </template>
 
 <script>
+import axios from "axios";
+import YoutubeListItem from "@/components/YoutubeListItem.vue";
+
 const API_KEY = "AIzaSyA5sx2tkiELZ9R2oSoVdCSJDSj8jHUvqIw";
 const API_URL = "https://www.googleapis.com/youtube/v3/search";
-export default {};
+
+export default {
+  name: "YoutubeList",
+  components: {
+    YoutubeListItem,
+  },
+  data() {
+    return {
+      keyword: "",
+      videos: [],
+    };
+  },
+  methods: {
+    searchVideo() {
+      if (!this.keyword) {
+        alert("검색어를 입력하세요!");
+      } else {
+        axios
+          .get(API_URL, {
+            params: {
+              key: API_KEY,
+              type: "video",
+              part: "snippet",
+              q: this.keyword,
+            },
+          })
+          .then((response) => {
+            this.videos = response.data.items;
+            console.log(this.keyword);
+            console.log(response);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }
+    },
+  },
+};
 </script>
 
 <style></style>
