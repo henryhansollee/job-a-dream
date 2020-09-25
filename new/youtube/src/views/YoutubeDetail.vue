3ff263257214c18375d2ca5546b2a9f2fb58f1d8
임기람 <rlfka@naver.com>
2020-09-25 17:28:54 +0900

    get videos from youtube
    Former-commit-id: 6ab333d934262114ef34288e08978f801adf8ffa

---
+++ new/youtube/src/views/YoutubeDetail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="col-3">
+      <!-- <iframe
+        :src="iframeUrl"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe> -->
+    </div>
+    <YoutubeListItem @giveVideo="getVideo(id)" />
+  </div>
+</template>
+
+<script>
+// import axios from "axios";
+import YoutubeListItem from "@/components/YoutubeListItem.vue";
+
+export default {
+  name: "YoutubeDetail",
+  components: {
+    YoutubeListItem,
+  },
+  computed: {
+    // iframeUrl(video) {
+    //   console.log(video, "하이");
+    //   return `http://youtube.com/embed/${video.id.videoId}`;
+    // },
+  },
+  methods: {
+    getVideo(video) {
+      console.log(video, "뀨");
+    },
+  },
+};
+</script>
+
+<style></style>
