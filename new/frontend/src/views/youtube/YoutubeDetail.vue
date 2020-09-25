7911cbee3e28fe036befe9942e115e5b41d108cd
임기람 <rlfka@naver.com>
2020-09-25 17:46:49 +0900

    sketch YoutubeDetail
    Former-commit-id: 1acc69fdada2c21b2ab0ee8a0b0bd7827405ae4f

--- new/frontend/src/views/youtube/YoutubeDetail.vue
+++ new/frontend/src/views/youtube/YoutubeDetail.vue
@@ -1,11 +1,38 @@
 <template>
   <div>
-    <h1>유튜브 상세</h1>
+    <div class="col-3">
+      <!-- <iframe
+        :src="iframeUrl"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe> -->
+    </div>
+    <YoutubeListItem @giveVideo="getVideo(id)" />
   </div>
 </template>
 
 <script>
-export default {};
+// import axios from "axios";
+import YoutubeListItem from "@/components/YoutubeListItem.vue";
+
+export default {
+  name: "YoutubeDetail",
+  components: {
+    YoutubeListItem,
+  },
+  computed: {
+    // iframeUrl(video) {
+    //   console.log(video, "하이");
+    //   return `http://youtube.com/embed/${video.id.videoId}`;
+    // },
+  },
+  methods: {
+    getVideo(video) {
+      console.log(video, "뀨");
+    },
+  },
+};
 </script>
 
 <style></style>
