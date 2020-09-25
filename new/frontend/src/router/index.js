0635a380fd03bebf7ecf2f9636bd2ac7fc01518c
이한솔 <henryhansollee@gmail.com>
2020-09-25 14:24:41 +0900

    Create lists and link router
    Former-commit-id: 5783f001a3ab17d7d6de90adadbed52515da96c2

--- new/frontend/src/router/index.js
+++ new/frontend/src/router/index.js
@@ -1,23 +1,52 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
+// Home
 import Home from '../views/Home.vue'
+
+// Lists
+import AudioListView from '../views/lists/AudioListView'
+import CoverLetterListView from '../views/lists/CoverLetterListView'
+import FullCourseListView from '../views/lists/FullCourseListView'
+import VideoListView from '../views/lists/VideoListView'
+import YoutubeListView from '../views/lists/YoutubeListView'
 
 Vue.use(VueRouter)
 
 const routes = [
+  // Home
   {
     path: '/',
     name: 'Home',
     component: Home
   },
+
+  // Lists
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/audio/list',
+    name: 'AudioListView',
+    component: AudioListView
+  },
+  {
+    path: '/coverletter/list',
+    name: 'CoverLetterListView',
+    component: CoverLetterListView
+  },
+  {
+    path: '/fullcourse/list',
+    name: 'FullCourseListView',
+    component: FullCourseListView
+  },
+  {
+    path: '/video/list',
+    name: 'VideoListView',
+    component: VideoListView
+  },
+  {
+    path: '/youtube/list',
+    name: 'YoutubeListView',
+    component: YoutubeListView
+  },
 ]
 
 const router = new VueRouter({
