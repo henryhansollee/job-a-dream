a19581a1318c0cf0af2047d9c528dd8c301beb62
임기람 <rlfka@naver.com>
2020-09-25 17:36:58 +0900

    make a component of YoutubeList
    Former-commit-id: e1233c0ada6c58ac27262f04a7f0718624264f41

---
+++ new/frontend/src/components/youtube/YoutubeListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container mt-5" @click="goDetail">
+    <!-- <router-link
+      :to="`/${video.id.videoId}`"
+      style="text-decoration:none; color: black;"
+    > -->
+    <div class="play-video">
+      <img :src="video.snippet.thumbnails.medium.url" alt="" class="videoImg" />
+      <div class="play">
+        <div
+          style="
+            border-radius: 15px;
+            background-color: green;
+            color: white;
+            padding: 10px 15px;
+          "
+        >
+          <i class="fas fa-play"></i> 재생
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <h5>{{ video.snippet.title }}</h5>
+    </div>
+    <!-- </router-link> -->
+  </div>
+</template>
