cb03c537a881f014e40bbffa548e9332b4350687
임기람 <rlfka@naver.com>
2020-09-25 17:33:18 +0900

    get youtubeKEY
    Former-commit-id: 71a2e5c34217ea8048617a232c34bbbbb954c62f

--- new/frontend/src/views/youtube/YoutubeList.vue
+++ new/frontend/src/views/youtube/YoutubeList.vue
@@ -1,10 +1,26 @@
 <template>
   <div>
-    <h1>유튜브 리스트</h1>
+    <h3>면접 참고 영상입니다!</h3>
+    <input
+      type="text"
+      placeholder="search"
+      v-model="keyword"
+      @keypress.enter="searchVideo"
+    />
+    <div class="d-flex ml-5">
+      <YoutubeListItem
+        v-for="video in videos"
+        :key="video.etag"
+        :video="video"
+        :keyword="keyword"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+const API_KEY = "AIzaSyA5sx2tkiELZ9R2oSoVdCSJDSj8jHUvqIw";
+const API_URL = "https://www.googleapis.com/youtube/v3/search";
 export default {};
 </script>
 
