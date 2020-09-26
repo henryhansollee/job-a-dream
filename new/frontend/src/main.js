ce3e6b450cf60a2c40df415cfa8189cde75cc657
이한솔 <henryhansollee@gmail.com>
2020-09-26 17:10:25 +0900

    Update favicon
    Former-commit-id: e9b73ef2eca8f44ff097a506688f31bfdbfc331a

--- new/frontend/src/main.js
+++ new/frontend/src/main.js
@@ -5,6 +5,8 @@ import vuetify from './plugins/vuetify';
 import store from './store'
 import VueFullPage from 'vue-fullpage.js'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 Vue.use(VueFullPage);
