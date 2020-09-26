8ed7d728b9b5dd191b10263fce2a47394c7a7c03
이한솔 <henryhansollee@gmail.com>
2020-09-26 17:33:16 +0900

    Apply global css
    Former-commit-id: 2588d938cedfbbf9a11f859762f63b0951d5662b

--- new/frontend/src/main.js
+++ new/frontend/src/main.js
@@ -7,6 +7,7 @@ import VueFullPage from 'vue-fullpage.js'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "@/assets/css/global.css";
 
 Vue.config.productionTip = false
 Vue.use(VueFullPage);
