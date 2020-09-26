c8c6060a854c8dee21e637f39128b002b73c22b4
이한솔 <henryhansollee@gmail.com>
2020-09-26 14:35:01 +0900

    Apply fullpage
    Former-commit-id: e96a42f709c663938fc36fb1a5bf3908697bca50

--- new/frontend/src/App.vue
+++ new/frontend/src/App.vue
@@ -1,7 +1,29 @@
 <template>
   <div>
     <div v-if="!isLoggedIn">
-      <Login />
+      <full-page ref="fullpage" :options="options">
+      <div class="section home-section1">
+        <button class="next next-button1" @click="$refs.fullpage.api.moveSectionDown()">
+          <i class="fas fa-chevron-down"></i>
+        </button>
+        <HomeSection1 />
+      </div>
+      <div class="section home-section2">
+        <button class="next next-button2" @click="$refs.fullpage.api.moveSectionDown()">
+          <i class="fas fa-chevron-down"></i>
+        </button>
+        <HomeSection2 />
+      </div>
+      <div class="section home-section3">
+        <button class="next next-button3" @click="$refs.fullpage.api.moveSectionDown()">
+          <i class="fas fa-chevron-down"></i>
+        </button>
+        <HomeSection3 />
+      </div>
+      <div class="section home-section4">
+        <HomeSection4 />
+      </div>
+    </full-page>
     </div>
 
   <v-app v-else id="inspire">
@@ -187,24 +209,41 @@
 
 <script>
 import { mapGetters } from "vuex";
-import Login from '@/components/auth/Login'
 import Logout from '@/components/auth/Logout'
+import HomeSection1 from "@/components/home/HomeSection1";
+import HomeSection2 from "@/components/home/HomeSection2";
+import HomeSection3 from "@/components/home/HomeSection3";
+import HomeSection4 from "@/components/home/HomeSection4";
+
 
   export default {
     components: {
-      Login,
       Logout,
+      HomeSection1,
+      HomeSection2,
+      HomeSection3,
+      HomeSection4,
     },
     data() {
       return {
         drawer: null,
         dialogUser: false,
         dialogQuestion: false,
+        options: {
+          afterLoad: this.afterLoad,
+          controlArrows: true,
+          scrollBar: true,
+        },
       }
     },
     computed: {
       ...mapGetters(["isLoggedIn"]),
     },
+    methods: {
+      moveSectionDown() {
+        this.$refs.fullpage.$fullpage.moveSectionDown();
+      },
+    }
   }
 </script>
 
@@ -213,5 +252,43 @@ import Logout from '@/components/auth/Logout'
   width: 170px;
   border: solid 1px;
   border-radius: 50%;
+}
+.home-section1 {
+  background-color: #fcbe32;
+}
+.home-section2 {
+  background-color: #fffcf0;
+}
+.home-section3 {
+  background-color: #5e5e5f;
+}
+.home-section4 {
+  background-color: #a79c8e;
+}
+.next {
+  border: none;
+  outline: none;
+  border-radius: 3px;
+  padding: 5px;
+  background-color: transparent;
+}
+.next:focus {
+  border: none;
+  outline: none;
+}
+.next-button1 {
+  position: absolute;
+  top: 22%;
+  left: 50%;
+}
+.next-button2 {
+  position: absolute;
+  top: 47%;
+  left: 50%;
+}
+.next-button3 {
+  position: absolute;
+  top: 72%;
+  left: 50%;
 }
 </style>
---
+++ new/frontend/src/components/home/HomeSection1.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="d-flex justify-content-between align-items-center inner-box">
+    <img src="@/assets/img/home-logo.jpg" alt="home-logo" class="home-logo mr-5" />
+    <Login class="ml-5" />
+  </div>
+</template>
+
+<script>
+import Login from "../auth/Login";
+
+export default {
+  name: "HomeSection1",
+  components: {
+    Login,
+  },
+};
+</script>
+
+<style>
+.inner-box {
+  height: 100%;
+  padding-left: 15%;
+  padding-right: 15%;
+}
+.home-logo {
+  width: 40%;
+}
+</style>
