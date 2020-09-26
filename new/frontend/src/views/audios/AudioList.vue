929dda8abca73358285a541cb29cc1c28855c2df
이한솔 <henryhansollee@gmail.com>
2020-09-26 23:58:38 +0900

    Add temp thumbnails
    Former-commit-id: ce11fa208e6ca1da95b294c77a6ae3e7328ce4de

--- new/frontend/src/views/audios/AudioList.vue
+++ new/frontend/src/views/audios/AudioList.vue
@@ -40,7 +40,7 @@
           <!-- 데이터 넣어야함 -->
           <v-card>
             <v-img
-              src="https://cdn.vuetifyjs.com/images/cards/plane.jpg"
+              src="@/assets/thumbnails/audio-thumbnail.jpg"
               class="white--text align-end"
               gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
               height="200px"
@@ -62,7 +62,7 @@
           <!-- 데이터 넣어야함 -->
           <v-card>
             <v-img
-              src="https://cdn.vuetifyjs.com/images/cards/plane.jpg"
+              src="@/assets/thumbnails/audio-thumbnail.jpg"
               class="white--text align-end"
               gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
               height="200px"
@@ -84,7 +84,7 @@
           <!-- 데이터 넣어야함 -->
           <v-card>
             <v-img
-              src="https://cdn.vuetifyjs.com/images/cards/plane.jpg"
+              src="@/assets/thumbnails/audio-thumbnail.jpg"
               class="white--text align-end"
               gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
               height="200px"
@@ -106,7 +106,7 @@
           <!-- 데이터 넣어야함 -->
           <v-card>
             <v-img
-              src="https://cdn.vuetifyjs.com/images/cards/plane.jpg"
+              src="@/assets/thumbnails/audio-thumbnail.jpg"
               class="white--text align-end"
               gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
               height="200px"
@@ -128,7 +128,73 @@
           <!-- 데이터 넣어야함 -->
           <v-card>
             <v-img
-              src="https://cdn.vuetifyjs.com/images/cards/plane.jpg"
+              src="@/assets/thumbnails/audio-thumbnail.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title>
+                제목
+              </v-card-title>
+            </v-img>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-card-text>
+                #태그
+              </v-card-text>
+            </v-card-actions>
+          </v-card>
+          <!-- 데이터 넣어야함 -->
+        </v-col>
+        <v-col cols="3">
+          <!-- 데이터 넣어야함 -->
+          <v-card>
+            <v-img
+              src="@/assets/thumbnails/audio-thumbnail.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title>
+                제목
+              </v-card-title>
+            </v-img>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-card-text>
+                #태그
+              </v-card-text>
+            </v-card-actions>
+          </v-card>
+          <!-- 데이터 넣어야함 -->
+        </v-col>
+        <v-col cols="3">
+          <!-- 데이터 넣어야함 -->
+          <v-card>
+            <v-img
+              src="@/assets/thumbnails/audio-thumbnail.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title>
+                제목
+              </v-card-title>
+            </v-img>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-card-text>
+                #태그
+              </v-card-text>
+            </v-card-actions>
+          </v-card>
+          <!-- 데이터 넣어야함 -->
+        </v-col>
+        <v-col cols="3">
+          <!-- 데이터 넣어야함 -->
+          <v-card>
+            <v-img
+              src="@/assets/thumbnails/audio-thumbnail.jpg"
               class="white--text align-end"
               gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
               height="200px"
