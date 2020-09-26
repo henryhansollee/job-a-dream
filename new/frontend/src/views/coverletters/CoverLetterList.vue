567246904637bcce138fd6eaaa7bb9cfa38a3796
이한솔 <henryhansollee@gmail.com>
2020-09-26 23:33:20 +0900

    Add temp lists
    Former-commit-id: bff78369c3b384bad509ea07b445cfbc100b4117

--- new/frontend/src/views/coverletters/CoverLetterList.vue
+++ new/frontend/src/views/coverletters/CoverLetterList.vue
@@ -2,6 +2,121 @@
   <div>
     <h1>자기소개서 리스트</h1>
     <router-link to="/coverletter/create">자기소개서 작성</router-link>
+
+    <v-container>
+      <v-row>
+        <v-col cols="3">
+          <!-- 데이터 넣어야함 -->
+          <v-card>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/plane.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title>
+                제목
+              </v-card-title>
+            </v-img>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-card-text>
+                #태그
+              </v-card-text>
+            </v-card-actions>
+          </v-card>
+          <!-- 데이터 넣어야함 -->
+        </v-col>
+        <v-col cols="3">
+          <!-- 데이터 넣어야함 -->
+          <v-card>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/plane.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title>
+                제목
+              </v-card-title>
+            </v-img>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-card-text>
+                #태그
+              </v-card-text>
+            </v-card-actions>
+          </v-card>
+          <!-- 데이터 넣어야함 -->
+        </v-col>
+        <v-col cols="3">
+          <!-- 데이터 넣어야함 -->
+          <v-card>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/plane.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title>
+                제목
+              </v-card-title>
+            </v-img>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-card-text>
+                #태그
+              </v-card-text>
+            </v-card-actions>
+          </v-card>
+          <!-- 데이터 넣어야함 -->
+        </v-col>
+        <v-col cols="3">
+          <!-- 데이터 넣어야함 -->
+          <v-card>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/plane.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title>
+                제목
+              </v-card-title>
+            </v-img>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-card-text>
+                #태그
+              </v-card-text>
+            </v-card-actions>
+          </v-card>
+          <!-- 데이터 넣어야함 -->
+        </v-col>
+        <v-col cols="3">
+          <!-- 데이터 넣어야함 -->
+          <v-card>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/plane.jpg"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="200px"
+            >
+              <v-card-title>
+                제목
+              </v-card-title>
+            </v-img>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-card-text>
+                #태그
+              </v-card-text>
+            </v-card-actions>
+          </v-card>
+          <!-- 데이터 넣어야함 -->
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
