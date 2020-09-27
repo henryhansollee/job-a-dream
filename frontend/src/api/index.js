7b2be87ec75dd3b83602a9b26b52ee53936fa532
이태환 <lwh1992@naver.com>
2020-09-27 22:39:15 +0900

    nginx for docker
    Former-commit-id: 0eaf647f5ea3508a9647b19de6313f726059f737

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,7 @@
 export default {
   URL: process.env.VUE_APP_BACKEND,
-  // URL: 'http://j3b107.p.ssafy.io:8000',
+  // URL: 'http://j3b107.p.ssafy.io:8080',
+  // URL: 'http://localhost:8080',
   ROUTES: {
     signup: "/accounts/signup/",
     login: "/accounts/login/",
