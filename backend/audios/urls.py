3db8de7490e8c9e5a4c382416311feffdce3faa6
최보근 <bogun11904@naver.com>
2020-09-28 00:53:48 +0900

    Feat audio service
    Former-commit-id: b60bc4be40c1101c60b35af8debd0f3778376a68

---
+++ backend/audios/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.VideoListAPI.as_view()),
+    path('<int:pk>', views.VideoDetailAPI.as_view()),
+    path('questions', views.QuestionAPI.as_view()),
+    path('questions/<int:pk>', views.QuestionAPI.as_view()),
+]
--- backend/interview/settings.py
+++ backend/interview/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'accounts',
     'boards',
     'videos',
+    'audios',
 ]
 
 MIDDLEWARE = [
