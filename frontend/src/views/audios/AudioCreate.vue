82310fe2851941768a6bc2a26625c23e89123b5b
임기람 <rlfka@naver.com>
2020-09-28 17:31:20 +0900

    change .env.development
    Former-commit-id: 7f235dead7b3609ea399b33c233c70c014c5accb

--- frontend/src/views/audios/AudioCreate.vue
+++ frontend/src/views/audios/AudioCreate.vue
@@ -2,31 +2,38 @@
   <div class="m-5">
     <v-stepper v-model="e1">
       <v-stepper-header>
-        <v-stepper-step :complete="e1 > 1" step="1">STEP 1. 질문 선택</v-stepper-step>
+        <v-stepper-step :complete="e1 > 1" step="1"
+          >STEP 1. 질문 선택</v-stepper-step
+        >
         <v-divider></v-divider>
-        <v-stepper-step :complete="e1 > 2" step="2">STEP 2. 음성 녹음</v-stepper-step>
+        <v-stepper-step :complete="e1 > 2" step="2"
+          >STEP 2. 음성 녹음</v-stepper-step
+        >
         <v-divider></v-divider>
         <v-stepper-step step="3">STEP 3. 정보 입력</v-stepper-step>
       </v-stepper-header>
       <v-stepper-items>
         <!-- 스텝 1 -->
         <v-stepper-content step="1">
-          <v-card class="mb-12" color="grey lighten-1" height="600px">
-
-          </v-card>
+          <v-card class="mb-12" color="grey lighten-1" height="600px"> </v-card>
           <v-btn color="primary" @click="e1 = 2">다음</v-btn>
         </v-stepper-content>
         <!-- 스텝 2 -->
         <v-stepper-content step="2">
           <v-card class="mb-12" color="grey lighten-1" height="600px">
             <!-- 오디오 -->
-            <dictaphone @stop="handleRecording($event)" mime-type="audio/wav"
-              v-slot="{ isRecording, startRecording, stopRecording }">
-                <button v-if="!isRecording" @click="startRecording">Start recording</button>
-                <button v-else @click="stopRecording">Stop recording</button>
+            <dictaphone
+              @stop="handleRecording($event)"
+              mime-type="audio/wav"
+              v-slot="{ isRecording, startRecording, stopRecording }"
+            >
+              <button v-if="!isRecording" @click="startRecording">
+                Start recording
+              </button>
+              <button v-else @click="stopRecording">Stop recording</button>
             </dictaphone>
 
-            <vue-dictaphone-spectrum-analyser/>
+            <vue-dictaphone-spectrum-analyser />
 
             <div v-if="audioSource">
               <audio :src="audioSource" controls></audio>
@@ -38,11 +45,24 @@
         <v-stepper-content step="3">
           <v-card class="mb-12" color="grey lighten-1" height="600px">
             <!-- 제목 -->
-            <input type="text" placeholder="title" v-model="audioData.title">
+            <input type="text" placeholder="title" v-model="audioData.title" />
             <!-- 태그 -->
             <div>
-              <b-form-tags v-model="audioData.update_tag" no-outer-focus class="mb-2">
-                <template v-slot="{ tags, inputAttrs, inputHandlers, tagVariant, addTag, removeTag }">
+              <b-form-tags
+                v-model="audioData.update_tag"
+                no-outer-focus
+                class="mb-2"
+              >
+                <template
+                  v-slot="{
+                    tags,
+                    inputAttrs,
+                    inputHandlers,
+                    tagVariant,
+                    addTag,
+                    removeTag,
+                  }"
+                >
                   <b-input-group class="mb-2">
                     <b-form-input
                       v-bind="inputAttrs"
@@ -51,7 +71,9 @@
                       class="form-control"
                     ></b-form-input>
                     <b-input-group-append>
-                      <b-button @click="addTag()" variant="primary">Add</b-button>
+                      <b-button @click="addTag()" variant="primary"
+                        >Add</b-button
+                      >
                     </b-input-group-append>
                   </b-input-group>
                   <div class="d-inline-block" style="font-size: 1.5rem;">
@@ -62,7 +84,8 @@
                       :title="tag"
                       :variant="tagVariant"
                       class="mr-1"
-                    >{{ tag }}</b-form-tag>
+                      >{{ tag }}</b-form-tag
+                    >
                   </div>
                 </template>
               </b-form-tags>
@@ -77,7 +100,7 @@
 
 <script>
 import { mapActions } from "vuex";
-import Dictaphone from '@/components/audios/Dictaphone';
+import Dictaphone from "@/components/audios/Dictaphone";
 
 export default {
   name: "AudioCreate",
@@ -87,16 +110,16 @@ export default {
   data() {
     return {
       audioData: {
-        title: '',
-        audio_file: '',
+        title: "",
+        audio_file: "",
         update_tag: [],
       },
-      audioSource: '',
+      audioSource: "",
       e1: 1,
     };
   },
   methods: {
-    ...mapActions([ "createAudio" ]),
+    ...mapActions(["createAudio"]),
     handleRecording({ blob, src }) {
       this.audioData.audio_file = blob;
       this.audioSource = src;
@@ -104,15 +127,17 @@ export default {
     createAudioFormData() {
       const audioFormData = new FormData();
       const audio_file_name = Date.now();
-      audioFormData.append('title', this.audioData.title);
-      audioFormData.append('audio_file', this.audioData.audio_file, audio_file_name);
-      audioFormData.append('update_tag', this.audioData.update_tag);
+      audioFormData.append("title", this.audioData.title);
+      audioFormData.append(
+        "audio_file",
+        this.audioData.audio_file,
+        audio_file_name
+      );
+      audioFormData.append("update_tag", this.audioData.update_tag);
       this.createAudio(audioFormData);
     },
   },
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
