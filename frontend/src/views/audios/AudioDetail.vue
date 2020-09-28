3de21ccb21b334123285e6db9d825dac95853345
임기람 <rlfka@naver.com>
2020-09-28 21:35:38 +0900

    check audio detail
    Former-commit-id: 1b26013f5ae434a2034f67be311eba4f9739a6a9

--- frontend/src/views/audios/AudioDetail.vue
+++ frontend/src/views/audios/AudioDetail.vue
@@ -1,15 +1,48 @@
 <template>
   <div>
-    <h1>음성 분석 결과</h1>
+    <h4>{{ audioResult.title }}</h4>
+    <h5>{{ audioResult.question }}</h5>
+    <audio controls :src="audioResult.audio_file"></audio>
+    <h6>{{ cutDate(audioResult.created_at) }}</h6>
+    <div v-for="tag in audioResult.tag" :key="tag">#{{ tag }}</div>
+
+    <router-link class="text-decoration-none" to="/audios/list">
+      <v-btn medium color="warning" dark>목록으로</v-btn>
+    </router-link>
+
+    <v-btn medium color="danger" dark @click="deleteAudio(audioResult.id)"
+      >삭제</v-btn
+    >
   </div>
 </template>
 
 <script>
-export default {
+import { mapState, mapActions } from "vuex";
 
-}
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(["audioResult"]),
+  },
+  methods: {
+    ...mapActions(["getAudioResult", "deleteAudio"]),
+    cutDate(date) {
+      let CD = date + "";
+      const year = CD.substring(0, 4) + ".";
+      const month = CD.substring(5, 7) + ".";
+      const day = CD.substring(8, 10) + "  ";
+      const hour = CD.substring(11, 13) + ":";
+      const minute = CD.substring(14, 16) + "";
+      const res = year + month + day + hour + minute;
+      return res;
+    },
+  },
+  created() {
+    this.getAudioResult(this.$route.params.id);
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
