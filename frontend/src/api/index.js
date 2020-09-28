abf305016ba9035ff15f5d8eef2339e647beaaf2
이태환 <lwh1992@naver.com>
2020-09-28 15:36:35 +0900

    nginx 추가 변경사항
    Former-commit-id: 59f6030b59b6598de01d8de8a9b359b0999409c5

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,12 +1,7 @@
 export default {
   URL: process.env.VUE_APP_BACKEND,
-<<<<<<< HEAD
   // URL: 'http://j3b107.p.ssafy.io:8080',
   // URL: 'http://localhost:8080',
-=======
-  // URL: 'http://j3b107.p.ssafy.io:8000',
-
->>>>>>> 8f8c9f27f0aa5b87e48ffa87c63cbaade962c929
   ROUTES: {
     signup: "/accounts/signup/",
     login: "/accounts/login/",
