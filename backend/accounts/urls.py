c584e0a4a7f15ab24c42289b72df5ba5b50982d3
최보근 <bogun11904@naver.com>
2020-09-28 18:42:49 +0900

    Modify get user infomations
    Former-commit-id: 52b76d68c2b060876b6f21c1956e0cced45fdd10

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import UserList, UserProfile
+from .views import UserList, UserProfile, UserInfo
 from .serializers import CustomJWTSerializer
 from rest_framework_jwt.views import ObtainJSONWebToken
 
 urlpatterns = [
     path('signup/', UserList.as_view()),
     path('login/', ObtainJSONWebToken.as_view(serializer_class=CustomJWTSerializer)),
+    path('', UserInfo.as_view()),
     path('<int:pk>', UserProfile.as_view()),
 ]
