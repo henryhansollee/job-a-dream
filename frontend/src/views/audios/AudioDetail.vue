05ad9ca73050c191d5dc61784a2eb17ba7c2a9a5
임기람 <rlfka@naver.com>
2020-09-30 01:32:39 +0900

    design Audio Detail Page
    Former-commit-id: 98f30675847b5750ff993e292825df55a0f09df3

--- frontend/src/views/audios/AudioDetail.vue
+++ frontend/src/views/audios/AudioDetail.vue
@@ -1,50 +1,62 @@
 <template>
   <div class="p-5" style="">
-    <div class="d-flex flex-row" style="">
-      <audio controls :src="audioResult.audio_file"></audio>
-      <div
-        class="ml-5 d-flex flex-column justify-content-between"
-        style="padding-top:3px;width:100%;"
-      >
-        <div style="">
-          <div class="d-flex justify-content-between">
-            <h4>{{ audioResult.title }}</h4>
-            <div class="dropdown dropleft mr-5">
-              <button type="button" class="comment-btn" data-toggle="dropdown">
-                <i class="fas fa-ellipsis-v"></i>
-              </button>
-              <div class="dropdown-menu">
-                <a
-                  class="dropdown-item"
-                  style="color:black;"
-                  @click="deleteAudio(audioResult.id)"
-                  >삭제</a
-                >
-              </div>
+    <div
+      class="ml-5 d-flex flex-column justify-content-between"
+      style="padding-top:3px;width:100%;"
+    >
+      <div style="">
+        <div class="d-flex justify-content-between">
+          <h4>{{ audioResult.title }}</h4>
+          <div class="dropdown dropleft mr-5">
+            <button type="button" class="comment-btn" data-toggle="dropdown">
+              <i class="fas fa-ellipsis-v"></i>
+            </button>
+            <div class="dropdown-menu">
+              <a
+                class="dropdown-item"
+                style="color:black;"
+                @click="deleteAudio(audioResult.id)"
+                >삭제</a
+              >
             </div>
           </div>
-          <h6 style="color:gray;font-size:14.5px;">
-            {{ cutDate(audioResult.created_at) }}
-          </h6>
-          <!--질문 뽑아와야 됨-->
-          <h5 class="mt-5" style="font-size:x-large;">
-            질문: {{ audioResult.question }}
-          </h5>
-          <div
-            v-for="tag in cutTag(audioResult.tag)"
-            :key="tag"
-            style="display:inline; font-size:large;"
-          >
-            {{ tag }}
-          </div>
-          <br />
         </div>
-        <div class="mr-5" style="">
-          <div class="d-flex justify-content-end">
-            <router-link class="text-decoration-none" to="/audios/list">
-              <v-btn medium color="warning" dark>목록으로</v-btn>
-            </router-link>
-          </div>
+        <h6 style="color:gray;font-size:14.5px;">
+          {{ cutDate(audioResult.created_at) }}
+        </h6>
+        <!--질문 뽑아와야 됨-->
+        <h5 class="mt-5" style="font-size:x-large;">
+          질문: {{ audioResult.question }}
+        </h5>
+        <div
+          v-for="tag in cutTag(audioResult.tag)"
+          :key="tag"
+          style="display:inline; font-size:large;"
+        >
+          {{ tag }}
+        </div>
+        <p />
+        <audio controls :src="audioResult.audio_file"></audio>
+      </div>
+      <div style="width:800px; height:250px; border:3px solid red;">
+        대본 들어갈 공간
+      </div>
+      <div
+        class="d-flex flex-row justify-content-between"
+        style="width:800px; height:250px; border:3px solid blue;"
+      >
+        <div style="width:400px; height:250px; border:3px solid green;">
+          워드 클라우드
+        </div>
+        <div style="width:400px; height:250px; border:3px solid orange;">
+          발음 정확도
+        </div>
+      </div>
+      <div class="mr-5 mt-5" style="">
+        <div class="d-flex justify-content-end">
+          <router-link class="text-decoration-none" to="/audios/list">
+            <v-btn medium color="warning" dark>목록으로</v-btn>
+          </router-link>
         </div>
       </div>
     </div>
@@ -72,6 +84,17 @@ export default {
       const minute = CD.substring(14, 16) + "";
       const res = year + month + day + hour + minute;
       return res;
+    },
+    cutTag(tags) {
+      if (tags) {
+        let result = [];
+        let arr = tags[0].split(",");
+        for (let tag of arr) {
+          let res = "#" + tag;
+          result.push(res);
+        }
+        return result;
+      }
     },
   },
   created() {
