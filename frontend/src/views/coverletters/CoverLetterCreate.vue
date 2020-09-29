cd702bee4f7c94dbb9c7ba45164bab3303855902
이한솔 <henryhansollee@gmail.com>
2020-09-29 16:43:20 +0900

    Update coverletter create UI
    Former-commit-id: 99552b5e60b04e15a6b617186555e01710c61794

--- frontend/src/views/coverletters/CoverLetterCreate.vue
+++ frontend/src/views/coverletters/CoverLetterCreate.vue
@@ -2,47 +2,32 @@
   <div class="m-5">
     <v-stepper v-model="e1">
       <v-stepper-header>
-        <v-stepper-step :complete="e1 > 1" step="1"
-          >STEP 1. 자기소개서 항목 입력</v-stepper-step
-        >
+        <v-stepper-step :complete="e1 > 1" step="1">STEP 1. 자기소개서 입력</v-stepper-step>
         <v-divider></v-divider>
-        <v-stepper-step :complete="e1 > 2" step="2"
-          >STEP 2. 자기소개서 내용 입력</v-stepper-step
-        >
-        <v-divider></v-divider>
-        <v-stepper-step step="3">STEP 3. 정보 입력</v-stepper-step>
-        <v-divider></v-divider>
+        <v-stepper-step step="2">STEP 2. 정보 입력</v-stepper-step>
       </v-stepper-header>
-
       <v-stepper-items>
         <!--자소서 subject 입력-->
         <v-stepper-content step="1">
-          <v-card class="mb-12" color="grey lighten-1" height="600px">
-            <!-- 항목 -->
-            <input
-              type="text"
-              placeholder="subject"
-              v-model="coverletterData.subject"
-            />
+          <h3 class="text-center m-4">자기소개서를 입력해주세요.</h3>
+          <v-card class="mx-auto" max-width="700" min-height="300" tile>
+            <div class="w-100 d-flex flex-column align-items-center">
+              <label for="input-with-list1" class="mt-5">항목</label>
+              <b-form-input class="w-50" list="input-list1" id="input-with-list" type="text" placeholder="항목을 입력해주세요." v-model="coverletterData.subject"></b-form-input>
+            </div>
+            <div class="w-100 d-flex flex-column align-items-center">
+              <label for="input-with-list2" class="mt-5">내용</label>
+              <b-form-textarea class="w-50" list="input-list2" id="input-with-list" type="text" placeholder="내용을 입력해주세요." v-model="coverletterData.content"></b-form-textarea>
+            </div>
           </v-card>
-          <v-btn color="primary" @click="e1 = 2">다음</v-btn>
-        </v-stepper-content>
-
-        <!--자소서 content 입력-->
-        <v-stepper-content step="2">
-          <v-card class="mb-12" color="grey lighten-1" height="600px">
-            <!-- 내용 -->
-            <input
-              type="text"
-              placeholder="content"
-              v-model="coverletterData.content"
-            />
-          </v-card>
-          <v-btn color="primary" @click="e1 = 3">다음</v-btn>
+          <div class="w-100 d-flex flex-column">
+            <v-btn v-if="coverletterData.subject && coverletterData.content" class="align-self-center m-4 w-25" color="primary" @click="e1 = 2">다음</v-btn> 
+            <v-btn v-else class="align-self-center m-4 w-25" color="primary" depressed disabled>다음</v-btn>
+          </div>
         </v-stepper-content>
 
         <!--제목,태그 입력-->
-        <v-stepper-content step="3">
+        <v-stepper-content step="2">
           <v-card class="mb-12" color="grey lighten-1" height="600px">
             <input
               type="text"
