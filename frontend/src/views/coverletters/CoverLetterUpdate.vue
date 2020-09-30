1b60778f8587dad8709c0198fb85be3aee79bab0
임기람 <rlfka@naver.com>
2020-09-30 13:29:16 +0900

    modify tag design
    Former-commit-id: 4ec8247495d52bac0bf79e35deaf1fa7f40b1d38

--- frontend/src/views/coverletters/CoverLetterUpdate.vue
+++ frontend/src/views/coverletters/CoverLetterUpdate.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="p-5" style="">
     <!-- {{ coverletterResult }} -->
-    <label for="cvletter-title" style="font-size:large;color:gray"
+    <label
+      for="cvletter-title"
+      class="ml-5 pl-1"
+      style="font-size:large;color:gray"
       >제목:
     </label>
     <input
@@ -12,7 +15,7 @@
       class="ml-1"
       style="border:2px solid gray; border-radius:4px;font-size:22.5px;"
     />
-    <div class="mt-5 p-1" style="border:3px solid blue;border-radius:5px;">
+    <div class="mt-5 p-1 ml-5" style="width:90%;">
       <div style="">
         <label
           for="cvletter-subject"
@@ -37,53 +40,58 @@
           style="font-size:18px;color:gray;"
           >자소서 내용 </label
         ><br />
-        <input
-          type="text"
-          id="cvletter-content"
+        <textarea
+          name="cvletter-content"
+          id=""
           v-model="coverletterResult.content"
           placeholder=" 내용을 입력하세요"
-          class="w-100"
-          style="border:2px solid gray; border-radius:4px;font-size:22.5px;"
-        />
+          style="border:3px solid gray;border-radius:5px;font-size:20px;width:100%;height:300px;"
+        ></textarea>
       </div>
     </div>
     <!-- 태그 -->
-    <div class="mt-5" style="border:3px solid orange; width:50%;">
-      <b-form-tags v-model="coverletterResult.tag" no-outer-focus class="mb-2">
-        <template
-          v-slot="{
-            tags,
-            inputAttrs,
-            inputHandlers,
-            tagVariant,
-            addTag,
-            removeTag,
-          }"
+    <div class="d-flex justify-content-center" style="">
+      <div style="width:60%" class="mr-5">
+        <b-form-tags
+          v-model="coverletterResult.tag"
+          no-outer-focus
+          class="mb-2"
         >
-          <b-input-group class="mb-2">
-            <b-form-input
-              v-bind="inputAttrs"
-              v-on="inputHandlers"
-              placeholder="New tag - Press enter to add"
-              class="form-control"
-            ></b-form-input>
-            <b-input-group-append>
-              <b-button @click="addTag()" variant="primary">Add</b-button>
-            </b-input-group-append>
-          </b-input-group>
-          <div class="d-inline-block" style="font-size: 1.5rem;">
-            <b-form-tag
-              v-for="tag in tags"
-              @remove="removeTag(tag)"
-              :key="tag"
-              :title="tag"
-              :variant="tagVariant"
-              class="mr-1"
-              >{{ tag }}</b-form-tag
-            >
-          </div>
-        </template>
-      </b-form-tags>
+          <template
+            v-slot="{
+              tags,
+              inputAttrs,
+              inputHandlers,
+              tagVariant,
+              addTag,
+              removeTag,
+            }"
+          >
+            <b-input-group class="mb-2">
+              <b-form-input
+                v-bind="inputAttrs"
+                v-on="inputHandlers"
+                placeholder="New tag - Press enter to add"
+                class="form-control"
+              ></b-form-input>
+              <b-input-group-append>
+                <b-button @click="addTag()" variant="primary">Add</b-button>
+              </b-input-group-append>
+            </b-input-group>
+            <div class="d-inline-block" style="font-size: 1.5rem;">
+              <b-form-tag
+                v-for="tag in tags"
+                @remove="removeTag(tag)"
+                :key="tag"
+                :title="tag"
+                :variant="tagVariant"
+                class="mr-1"
+                >{{ tag }}</b-form-tag
+              >
+            </div>
+          </template>
+        </b-form-tags>
+      </div>
     </div>
     <!-- <div v-for="tag in coverletterResult.tag" :key="tag">#{{ tag }}</div> -->
     <v-btn medium color="primary" dark @click="updateCV()">수정완료</v-btn>
