d33524d38b6af08bd2570a089209db2168f03dd9
임기람 <rlfka@naver.com>
2020-10-01 21:47:37 +0900

    add cvletter result in fullcourseDetail
    Former-commit-id: 434c0c9c5f159b7b323f1e8eca4e1e580ad445b8

--- frontend/src/views/fullcourses/FullCourseDetail.vue
+++ frontend/src/views/fullcourses/FullCourseDetail.vue
@@ -1,11 +1,120 @@
 <template>
-  <div>
-    <h1>풀코스 결과</h1>
+  <div class="p-5" style>
+    <div class style>
+      <!-- <video controls :src="fullcourseResult.video_file"></video> -->
+      <div
+        class="ml-5 d-flex flex-column justify-content-between"
+        style="padding-top:3px;width:100%;"
+      >
+        <div style>
+          <div class="d-flex justify-content-between">
+            <h4>{{ fullcourseResult.title }}</h4>
+            <div class="dropdown dropleft mr-5 pr-5">
+              <button
+                type="button"
+                class="mr-5 pr-5 basic-btn"
+                data-toggle="dropdown"
+              >
+                <i class="fas fa-ellipsis-v"></i>
+              </button>
+              <div class="dropdown-menu">
+                <a
+                  class="dropdown-item"
+                  style="color:black;"
+                  @click="deleteFullcourse(fullcourseResult.id)"
+                  >삭제</a
+                >
+              </div>
+            </div>
+          </div>
+          <h6 style="color:gray;font-size:14.5px;">
+            {{ cutDate(fullcourseResult.created_at) }}
+          </h6>
+          <div
+            v-for="tag in cutTag(fullcourseResult.tag)"
+            :key="tag"
+            style="display:inline; font-size:large;"
+          >
+            {{ tag }}
+          </div>
+          <!--자소서 분석 결과-->
+          <div
+            class="d-flex flex-row justify-content-around mt-4"
+            style="width:90%; height:250px; border:3px solid blue;"
+          >
+            <div style="width:48%; height:100%; border:3px solid green;">
+              <h5>{{ fullcourseResult.subject }}</h5>
+              <h5>{{ fullcourseResult.content }}</h5>
+            </div>
+            <div style="width:48%; height:100%; border:3px solid orange;">
+              맞춤법 검사 결과
+            </div>
+          </div>
+          <p />
+          <div>
+            <h5>예상질문</h5>
+            <div style="width:90%; height:200px; border:3px solid red;">
+              예상 질문 리스트
+            </div>
+          </div>
+          <!--질문 뽑아와야 됨-->
+          <h5 class="mt-5" style="font-size:x-large;">
+            질문: {{ fullcourseResult.question }}
+          </h5>
+          <br />
+        </div>
+        <div class="mr-5" style>
+          <div class="d-flex justify-content-end">
+            <router-link class="text-decoration-none" to="/fullcourses/list">
+              <v-btn class="basic-btn" medium color="warning" dark
+                >목록으로</v-btn
+              >
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { mapState, mapActions } from "vuex";
+
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(["fullcourseResult"]),
+  },
+  methods: {
+    ...mapActions(["getFullcourseResult", "deleteFullcourse"]),
+    cutDate(date) {
+      let CD = date + "";
+      const year = CD.substring(0, 4) + ".";
+      const month = CD.substring(5, 7) + ".";
+      const day = CD.substring(8, 10) + "  ";
+      const hour = CD.substring(11, 13) + ":";
+      const minute = CD.substring(14, 16) + "";
+      const res = year + month + day + hour + minute;
+      return res;
+    },
+    cutTag(tags) {
+      if (this.fullcourseResult.tag) {
+        let result = [];
+        let arr = tags[0].split(",");
+        for (let tag of arr) {
+          let res = "#" + tag;
+          result.push(res);
+        }
+        return result;
+      }
+    },
+  },
+  created() {
+    this.getFullcourseResult(this.$route.params.id);
+  },
+};
 </script>
 
 <style></style>
