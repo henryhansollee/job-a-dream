ff5e65925f72f495c44b07d65fdad238f23996c7
최보근 <bogun11904@naver.com>
2020-10-02 23:45:05 +0900

    modifying merge backend and stt
    Former-commit-id: 7492a9872aded629212b08a73427cb4302f5b0ff

--- frontend/src/views/audios/AudioCreate.vue
+++ frontend/src/views/audios/AudioCreate.vue
@@ -42,6 +42,7 @@
             class="mt-5"
               @stop="handleRecording($event)"
               mime-type="audio/wav"
+              samplerate=44100
               v-slot="{ isRecording, startRecording, stopRecording }"
             >
               <button class="btn btn-danger" v-if="!isRecording" @click="startRecording(selectedQ)">녹음시작</button>
