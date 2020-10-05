9dff1d2c4befe88d9de76a94d90a775b91a080c3
JAEHO <pyoun820@naver.com>
2020-10-05 14:35:34 +0900

    change stt file
    Former-commit-id: 2bc0e508d7c2205a16af1b429779ceab243e06c1

--- AI/AI_code/STT_API_version.py
+++ AI/AI_code/STT_API_version.py
@@ -1,33 +1,24 @@
 from google.cloud import speech
-from google.cloud.speech import enums
-import io
-import base64
 
 def transcribe_file(speech_file):
     client = speech.SpeechClient()
 
-    with io.open(speech_file, 'rb') as audio_file:
-        # content = audio_file.read()
-        data = audio_file.read()
-    # print('분석중....')
+    with open(speech_file, 'rb') as audio_file:
+        content = audio_file.read()
 
-    encData = base64.b64encode(data)
-    print(encData)
-
-    audio = {"content":encData}
+    audio = {"content":content}
     config = {
-        "encoding":enums.RecognitionConfig.AudioEncoding.LINEAR16,
         "model":"command_and_search",
         "enable_separate_recognition_per_channel":True,
-        "enable_automatic_punctuation": True,
         "audio_channel_count":2,
         "language_code":'ko-KR',
-        "sample_rate_hertz": 44100
     }
 
     response = client.recognize(config, audio)
-    # print(response.results[0].alternatives[0].transcript)
-    # print(response.results[0].alternatives[0].confidence)
-    print(response)
+    try:
+        print(response.results[0].alternatives[0].transcript)
+        print(response.results[0].alternatives[0].confidence)
+    except:
+        print('실패!')
 
-transcribe_file("1601643553055")
+transcribe_file("output.wav")
