53c748506c96f4f8af04d57c6986cc02cc6bfc48
임기람 <rlfka@naver.com>
2020-10-06 16:55:54 +0900

    comment in aboutUs
    Former-commit-id: b855c9f317938f4626b103db694035518af3b460

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    <h1>기본 페이지</h1>
+  <div class="m-5">
+    <div class="d-flex justify-content-around" style="height:400px; ">
+      <div class="d-flex flex-column mt-5" style="font-size:x-large;">
+        <div class="mt-5 mb-5">
+          면접이 부담스러워 긴장감이 높아진다면, <br /><br />
+          과도한 움직임이나 시선 처리가 산만해서 지적을 받았다면,
+          <br /><br />
+          발음이 부정확해서 고민이 많다면,
+        </div>
+        <div class="mt-5" style="font-size:xx-large;">
+          잡아드림이 다 봐드릴게요!
+        </div>
+      </div>
+      <div class="ml-5" style="">
+        <img src="@/assets/aboutUs/6.png" alt="" style="width:450px" />
+      </div>
+    </div>
   </div>
 </template>
 
