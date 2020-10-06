7e7d503e7f2ecc9857dbfb433462b6e762072e0c
임기람 <rlfka@naver.com>
2020-10-06 16:59:12 +0900

    second comment in aboutUs
    Former-commit-id: 8cb35d52910290eb2ad9f5b128d0892c2a1fa102

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -3,28 +3,28 @@
     <div class="d-flex justify-content-around" style="height:400px; ">
       <div class="d-flex flex-column mt-5" style="font-size:x-large;">
         <div class="mt-5 mb-5">
-          면접이 부담스러워 긴장감이 높아진다면, <br /><br />
-          과도한 움직임이나 시선 처리가 산만해서 지적을 받았다면,
-          <br /><br />
-          발음이 부정확해서 고민이 많다면,
+          면접이 부담스러워 긴장감이 높아진다면,
+          <br />
+          <br />과도한 움직임이나 시선 처리가 산만해서 지적을 받았다면,
+          <br />
+          <br />발음이 부정확해서 고민이 많다면,
         </div>
-        <div class="mt-5" style="font-size:xx-large;">
-          잡아드림이 다 봐드릴게요!
-        </div>
+        <div class="mt-5" style="font-size:xx-large;">잡아드림이 다 봐드릴게요!</div>
       </div>
-      <div class="ml-5" style="">
-        <img src="@/assets/aboutUs/6.png" alt="" style="width:450px" />
+      <div class="ml-5" style>
+        <img src="@/assets/aboutUs/6.png" alt style="width:450px" />
       </div>
+    </div>
+    <div class="d-flex flex-column align-items-center mb-5">
+      <div style="font-size:35px">구직자들이 제일 어려워하는 전형</div>
+      <div class="mb-5 ml-5" style="font-size:30px; padding-left:150px;">취업준비생 574명 대상</div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
