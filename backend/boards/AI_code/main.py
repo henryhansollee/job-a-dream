fefc635ecea730ebb93a5700bb0d4107ce258d7f
최보근 <bogun11904@naver.com>
2020-10-06 20:55:25 +0900

    Complete get questions at coverletter
    Former-commit-id: 24d85fac07d8c450ef8e6ec7e8ad42b032eee64b

--- backend/boards/AI_code/main.py
+++ backend/boards/AI_code/main.py
@@ -2,29 +2,50 @@ from google.cloud import translate_v2
 from . import aqgFunction
 from hanspell import spell_checker
 
-# Main Function
+def Check(string):
+    tmp = []
+    for i in range(len(string)):
+        if len(string[i]) > 2:
+            if hNvalidation(string[i]) == 1:
+                if ((string[i][0] == 'W' and string[i][1] == 'h') or (string[i][0] == 'H' and string[i][1] == 'o') or
+                        (string[i][0] == 'H' and string[i][1] == 'a')):
+                    WH = string[i].split(',')
+                    if len(WH) == 1:
+                        string[i] = string[i][:-1]
+                        string[i] = string[i][:-1]
+                        string[i] = string[i][:-1]
+                        string[i] = string[i] + "?"
+                        tmp.append(string[i])
+    return tmp
+
+
+def hNvalidation(sentence):
+    flag = 1
+
+    Length = len(sentence)
+    if Length > 4:
+        for i in range(Length):
+            if i + 4 < Length:
+                if sentence[i] == ' ' and sentence[i + 1] == 'h' and sentence[i + 2] == ' ' and sentence[
+                    i + 3] == 'N' and sentence[i + 4] == ' ':
+                    flag = 0
+    return flag
+
+
 def main(text):
-    # Create AQG object
     translate_client = translate_v2.Client()
     aqg = aqgFunction.AutomaticQuestionGenerator()
     kor_text = text
     eng_text = translate_client.translate(kor_text, target_language='en')['translatedText']
-    print(eng_text)
+    # eng_text = "Hello, I am Jaeho Pyun who applied to Samsung Electronics this time. "
+
     kor_question = []
     questionList = aqg.aqgParse(eng_text)
-    for q in questionList:
+    result_question = Check(questionList)
+    for q in result_question:
         string = translate_client.translate(q, target_language='ko')['translatedText']
         result_Q = spell_checker.check(string).checked
-        # print(result_Q)
-        # print(spell_checker.check(string).original)
         kor_question.append(result_Q)
-    # print(kor_question)
-    # aqg.display(kor_question)
-
+        # kor_question.append(q)
+    print(kor_question)
     return kor_question
-
-
-# Call Main Function
-if __name__ == "__main__":
-    main()
-
